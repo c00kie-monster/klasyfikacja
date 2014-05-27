6aad07d63552e51da00a3fe8b2ae1748652f4c9b
Damian <cookie.beast.mode@gmail.com>
2014-05-27 10:45:37 +0200

    Małe sprzatanie.

--- src/pl/lodz/p/kis/PR_GUI.java
+++ src/pl/lodz/p/kis/PR_GUI.java
@@ -970,9 +970,10 @@ public class PR_GUI extends javax.swing.JFrame {
 		return result;
     }
     
+    /**
+     * Bazowa klasa klasyfikatora.  
+     */
     private abstract class Classifier {
-    	
-    	
     	protected List<FileObject> allObjects = new ArrayList<>();
     	protected List<FileObject> classifiedObjects =  new ArrayList<>();
     	protected List<FileObject> unclassifiedObjects =  new ArrayList<>();
@@ -1044,8 +1045,6 @@ public class PR_GUI extends javax.swing.JFrame {
 	}
     
     private class NNClassifier extends Classifier {
-    	
-    	    	    	
     	public NNClassifier() {
     		super();
     	}
@@ -1098,7 +1097,6 @@ public class PR_GUI extends javax.swing.JFrame {
     				selectedDistances.put(classified, currentLength);
     			}
     			fitTheClass(unclassified);
-
     		}
        		
        		// Sprawdzamy ile udało się dobrze przyporządkować
@@ -1153,49 +1151,4 @@ public class PR_GUI extends javax.swing.JFrame {
 			}
 		}
     }
-    	
-
-    
 }
-
-//class Classifier {
-//    
-//    double[][] TrainingSet, TestSet;
-//    int[] ClassLabels;
-//    final int TRAIN_SET=0, TEST_SET=1;
-//    
-//    void generateTraining_and_Test_Sets(double[][] Dataset, String TrainSetSize){
-//
-//        int[] Index = new int[Dataset[0].length];
-//        double Th = Double.parseDouble(TrainSetSize)/100.0;
-//        int TrainCount=0, TestCount=0;
-//        for(int i=0; i<Dataset[0].length; i++) 
-//            if(Math.random()<=Th) {
-//                Index[i]=TRAIN_SET;
-//                TrainCount++;
-//            }
-//            else {
-//                Index[i]=TEST_SET;
-//                TestCount++;
-//            }   
-//        TrainingSet = new double[Dataset.length][TrainCount];
-//        TestSet = new double[Dataset.length][TestCount];
-//        TrainCount=0;
-//        TestCount=0;
-//        // label vectors for training/test sets
-//        for(int i=0; i<Index.length; i++){
-//            if(Index[i]==TRAIN_SET){
-//                System.arraycopy(Dataset[i], 0, TrainingSet[TrainCount++], 0, Dataset[0].length);
-//            }
-//            else
-//                System.arraycopy(Dataset[i], 0, TestSet[TestCount++], 0, Dataset[0].length);                
-//        }
-//    }
-//    
-//    protected void trainClissifier(double[][] TrainSet){
-//
-//    }
-//    
-//}
-
-
